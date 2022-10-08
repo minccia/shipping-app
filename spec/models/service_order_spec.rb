a6e7394886bca89775cbe8ae5bcedbff5f73a840
minccia <lusyvasilva@gmail.com>
2022-10-07 21:18:45 -0300

    Refactored unit tests to become more error-specific

--- spec/models/service_order_spec.rb
+++ spec/models/service_order_spec.rb
@@ -8,61 +8,81 @@ RSpec.describe ServiceOrder, type: :model do
       it 'false when sender_full_address is empty' do 
         order.sender_full_address = nil 
   
-        expect(order).to be_invalid
+        order.valid? 
+
+        expect(order.errors.include? :sender_full_address).to be_truthy
       end
   
       it 'false when sender_zip_code is empty' do 
         order.sender_zip_code = nil
   
-        expect(order).to be_invalid
+        order.valid? 
+
+        expect(order.errors.include? :sender_zip_code).to be_truthy
       end
   
       it 'false when package_height is empty' do 
         order.package_height = nil 
   
-        expect(order).to be_invalid
+        order.valid? 
+
+        expect(order.errors.include? :package_height).to be_truthy
       end
 
       it 'false when package_width is empty' do 
         order.package_width = nil 
 
-        expect(order).to be_invalid
+        order.valid? 
+
+        expect(order.errors.include? :package_width).to be_truthy
       end
 
       it 'false when package_depth is empty' do 
         order.package_depth = nil 
 
-        expect(order).to be_invalid
+        order.valid? 
+
+        expect(order.errors.include? :package_depth).to be_truthy
       end
 
       it 'false when package_weight is empty' do 
         order.package_weight = nil 
 
-        expect(order).to be_invalid
+        order.valid? 
+
+        expect(order.errors.include? :package_weight).to be_truthy
       end
 
       it 'false when receiver_name is empty' do 
         order.receiver_name = nil 
 
-        expect(order).to be_invalid
+        order.valid? 
+
+        expect(order.errors.include? :receiver_name).to be_truthy
       end
 
       it 'false when receiver_full_address is empty' do 
         order.receiver_full_address = nil 
 
-        expect(order).to be_invalid 
+        order.valid? 
+
+        expect(order.errors.include? :receiver_full_address).to be_truthy
       end
 
       it 'false when receiver_zip_code is empty' do 
         order.receiver_zip_code = nil 
 
-        expect(order).to be_invalid
+        order.valid? 
+
+        expect(order.errors.include? :receiver_zip_code).to be_truthy
       end
 
       it 'false when distance is empty' do 
         order.distance = nil 
 
-        expect(order).to be_invalid
+        order.valid? 
+
+        expect(order.errors.include? :distance).to be_truthy
       end
     end
   end
