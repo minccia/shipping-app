944949c0859f9a0e97effe0251a718fc1b765c57
minccia <lusyvasilva@gmail.com>
2022-10-09 15:08:21 -0300

    Updated seeds with pre-registered transport modality and vehicle

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,19 @@ User.destroy_all
 
 User.create!(name: 'common', email: 'common@sistemadefrete.com.br', password: '1234567')
 User.create!(name: 'admin', email: 'admin@sistemadefrete.com.br', password: '1234567', role: :admin)
+
+TransportModality.destroy_all
+
+trans_mod = TransportModality.create!(name: 'Expresso', 
+                                      maximum_distance: 100,
+                                      maximum_weight: 25,
+                                      fee: 59.9)
+
+Vehicle.destroy_all 
+
+Vehicle.create!(license_plate: 'ABC1D23',
+                brand_name: 'Fiat',
+                vehicle_type: 'Van',
+                fabrication_year: '2010',
+                maximum_capacity: '500',
+                transport_modality: trans_mod)
