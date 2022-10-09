07a10a1f8fc5630c4b53919f0f429034785dcea8
minccia <lusyvasilva@gmail.com>
2022-10-09 10:24:56 -0300

    Added helper method to check if attribute has minimum value required to minimize logic in views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   def formatted_zip_code(zip_code)
     "#{zip_code[0..4]}-#{zip_code[5..8]}"
-  end    
+  end 
+
+  def check_if_has_minimum(attribute)
+    return I18n.translate('no_minimum') if attribute.nil?
+    attribute
+  end
 end
