993762ca9b3c2eed1863bd400d9a5396072851d2
minccia <lusyvasilva@gmail.com>
2022-10-10 20:01:21 -0300

    Added unit tests to vehicle model

--- spec/factories/vehicles_factory.rb
+++ spec/factories/vehicles_factory.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
   factory :vehicle do
-    license_plate { "MyString" }
-    brand_name { "MyString" }
-    type { "" }
-    fabrication_year { "MyString" }
-    maximum_capacity { "MyString" }
-    status { 1 }
+    license_plate { Faker::Vehicle.license_plate }
+    brand_name { Faker::Vehicle.manufacture }
+    vehicle_type { Faker::Vehicle.car_type }
+    fabrication_year { Faker::Vehicle.year }
+    maximum_capacity { rand(500.3000) }
+    transport_modality { FactoryBot.create(:transport_modality) }
+    status { 0 }
   end
 end
--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,4 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
+  subject(:vehicle) { FactoryBot.build(:vehicle) }
+
+  describe '#valid?' do 
+    it 'false when license_plate is empty' do 
+      vehicle.license_plate = nil 
+
+      vehicle.valid?
+
+      expect(vehicle.errors.include? :license_plate).to be_truthy
+    end
+    
+    it 'false when brand_name is empty' do 
+      vehicle.brand_name = nil 
+  
+      vehicle.valid?
+  
+      expect(vehicle.errors.include? :brand_name).to be_truthy
+    end
+
+    it 'false when vehicle_type is empty' do 
+      vehicle.vehicle_type = nil 
+  
+      vehicle.valid?
+  
+      expect(vehicle.errors.include? :vehicle_type).to be_truthy
+    end
+
+    it 'false when fabrication_year is empty' do 
+      vehicle.fabrication_year = nil 
+  
+      vehicle.valid?
+  
+      expect(vehicle.errors.include? :fabrication_year).to be_truthy
+    end
+
+    it 'false when maximum_capacity is empty' do 
+      vehicle.maximum_capacity = nil 
+  
+      vehicle.valid?
+  
+      expect(vehicle.errors.include? :maximum_capacity).to be_truthy
+    end
+  end
 end
