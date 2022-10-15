150c5b6527a9f0aab7f49d335b023e85f361829c
minccia <lusyvasilva@gmail.com>
2022-10-15 18:13:30 -0300

    Added unit tests to fetch_table TableEntry model method

--- spec/models/table_entry_spec.rb
+++ spec/models/table_entry_spec.rb
@@ -39,4 +39,39 @@ RSpec.describe TableEntry, type: :model do
     end
 
   end
+
+  describe '#fetch_table' do 
+    it 'return correct table when table entry is from weight price table' do 
+      trans_mod = FactoryBot.create(:transport_modality)
+      table_entry = TableEntry.new(first_interval: 0,
+                                   second_interval: 50,
+                                   value: 30.0,
+                                   weight_price_table: trans_mod.weight_price_table
+                                  )
+                      
+      expect(table_entry.fetch_table).to eq trans_mod.weight_price_table
+    end
+
+    it 'return correct table when table entry is from distance price table' do 
+      trans_mod = FactoryBot.create(:transport_modality)
+      table_entry = TableEntry.new(first_interval: 0,
+                                   second_interval: 50,
+                                   value: 30.0,
+                                   distance_price_table: trans_mod.distance_price_table
+                                  )
+                      
+      expect(table_entry.fetch_table).to eq trans_mod.distance_price_table
+    end
+
+    it 'return correct table when table entry is from freight table' do 
+      trans_mod = FactoryBot.create(:transport_modality)
+      table_entry = TableEntry.new(first_interval: 0,
+                                   second_interval: 50,
+                                   value: 30.0,
+                                   freight_table: trans_mod.freight_table
+                                  )
+                      
+      expect(table_entry.fetch_table).to eq trans_mod.freight_table
+    end
+  end
 end
