2cd35fd027b4ff35e6d7dbf65dbc2676bbca27d6
minccia <lusyvasilva@gmail.com>
2022-10-17 20:39:31 -0300

    Added lateness explanation to service order search

--- spec/views/service_orders/search.html.erb_spec.rb
+++ spec/views/service_orders/search.html.erb_spec.rb
@@ -58,7 +58,7 @@ describe 'service_orders/search.html.erb' do
       expect(page).to have_content "Data estimada de entrega: #{service_order.started.due_date.hours.from_now.strftime("%d/%m/%Y")}"
     end
 
-    it 'and the service order is finished' do 
+    it 'and the service order is finished in time' do 
       allow(SecureRandom).to receive(:alphanumeric).with(15).and_return('ABCDE12345678') 
       service_order = FactoryBot.create(
                                         :service_order, status: :finished,
@@ -92,7 +92,53 @@ describe 'service_orders/search.html.erb' do
                                   value: value
                                 )
 
-      FinishedServiceOrder.create!(service_order: service_order, delivery_date: 4.days.from_now)
+      FinishedServiceOrder.create!(service_order: service_order, delivery_date: 1.day.from_now)
+
+      visit root_path 
+      fill_in 'Buscar ordem de serviço', with: 'ABC'
+      click_on 'Buscar'
+      
+      expect(page).to have_content 'Ordem de serviço <ABCDE12345678>'
+      expect(page).to have_content "Data de início: #{ (Date.today).strftime("%d/%m/%Y") }"
+      expect(page).to have_content "Data de encerramento: #{ 1.day.from_now.strftime("%d/%m/%Y") }"
+      expect(page).not_to have_content "Motivo do atraso:"
+    end
+
+    it 'and the service order is finished too late' do 
+      allow(SecureRandom).to receive(:alphanumeric).with(15).and_return('ABCDE12345678') 
+      service_order = FactoryBot.create(
+                                        :service_order, status: :finished,
+                                        distance: 80, package_weight: 20, created_at: Date.today
+                                      )
+
+      trans_mod = TransportModality.create!(name: 'Ghetto', 
+                                            maximum_distance: 100,
+                                            maximum_weight: 25,
+                                            fee: 12.9
+                                          )
+      vehicle = Vehicle.create!(
+                                license_plate: 'ABC1D23',
+                                brand_name: 'Fiat',
+                                vehicle_type: 'Van',
+                                fabrication_year: '2010',
+                                maximum_capacity: '500',
+                                transport_modality: trans_mod,
+                                status: :available
+                              )
+      TableEntry.create!(first_interval: 11, second_interval: 20, value: 0.50, weight_price_table_id: trans_mod.weight_price_table.id)
+      TableEntry.create!(first_interval: 60, second_interval: 80, value: 20, distance_price_table_id: trans_mod.distance_price_table.id)                                         
+      TableEntry.create!(first_interval: 50, second_interval: 100, value: 72, freight_table_id: trans_mod.freight_table.id)
+
+      value = trans_mod.so_execution_price(service_order)
+      due_date = trans_mod.so_execution_due_date(service_order)
+ 
+      StartedServiceOrder.create!(
+                                  service_order: service_order, vehicle: vehicle,
+                                  transport_modality: trans_mod, due_date: due_date,
+                                  value: value
+                                )
+      finished_so = FinishedServiceOrder.create!(service_order: service_order, delivery_date: 4.days.from_now)
+      LatenessExplanation.create(justification: 'Dormi demais oh', finished_service_order_id: finished_so.id)
 
       visit root_path 
       fill_in 'Buscar ordem de serviço', with: 'ABC'
@@ -101,6 +147,7 @@ describe 'service_orders/search.html.erb' do
       expect(page).to have_content 'Ordem de serviço <ABCDE12345678>'
       expect(page).to have_content "Data de início: #{ (Date.today).strftime("%d/%m/%Y") }"
       expect(page).to have_content "Data de encerramento: #{ 4.days.from_now.strftime("%d/%m/%Y") }"
+      expect(page).to have_content "Motivo do atraso: Dormi demais oh"
     end
   end
 end
