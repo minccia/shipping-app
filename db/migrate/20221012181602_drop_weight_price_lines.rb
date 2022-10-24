524783cfcd4aae217cc2f77b9caa486a73965e81
minccia <lusyvasilva@gmail.com>
2022-10-24 15:15:18 -0300

    Removed conflicting migrations what were breaking application setup

--- db/migrate/20221012181602_drop_weight_price_lines.rb
+++
@@ -1,5 +0,0 @@
-class DropWeightPriceLines < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :weight_price_lines
-  end
-end
--- db/migrate/20221012181731_drop_distance_price_lines.rb
+++
@@ -1,5 +0,0 @@
-class DropDistancePriceLines < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :distance_price_lines
-  end
-end
